eb8653b9442f708fc62a25326fbb567aa01a628c
funkemunky <funkemunkybiz@gmail.com>
2019-03-12 11:27:34 -0400

    Fix for ConcurrentModificationError on WrappedInUseEntityPacket
    This fix works by offloading the for loop, which finds the correct entity, onto the main thread. I believe this ConcurrentModificationError occured due to the list of entities grabbed by the Bukkit API is being written to when the entities are being updated every tick.
    
    This error would randomly pop up and is for the most part, harmles. However, it can be quite annoying for users. That being so, I set out to fixing it.

--- API/Atlas/src/java/cc/funkemunky/api/tinyprotocol/packet/in/WrappedInUseEntityPacket.java
+++ API/Atlas/src/java/cc/funkemunky/api/tinyprotocol/packet/in/WrappedInUseEntityPacket.java
@@ -1,18 +1,24 @@
 package cc.funkemunky.api.tinyprotocol.packet.in;
 
+import cc.funkemunky.api.Atlas;
 import cc.funkemunky.api.tinyprotocol.api.NMSObject;
 import cc.funkemunky.api.tinyprotocol.api.ProtocolVersion;
 import cc.funkemunky.api.tinyprotocol.reflection.FieldAccessor;
 import cc.funkemunky.api.tinyprotocol.reflection.MethodInvoker;
 import cc.funkemunky.api.utils.ReflectionsUtil;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 @Getter
 public class WrappedInUseEntityPacket extends NMSObject {
@@ -34,12 +40,25 @@ public class WrappedInUseEntityPacket extends NMSObject {
         id = Objects.requireNonNull(fetch(fieldId));
         Enum fieldAct = Objects.nonNull(fetch(fieldAction)) ? fetch(fieldAction) : null;
         action = fieldAct == null ? EnumEntityUseAction.INTERACT_AT : EnumEntityUseAction.valueOf(fieldAct.name());
-        List<Entity> entities = new ArrayList<>(player.getWorld().getEntities());
 
-        for (Entity ent : entities) {
-            if(id == ent.getEntityId()) {
-                entity = ent;
-                break;
+        FutureTask<Entity> task = new FutureTask<>(() -> {
+            List<Entity> entities = player.getWorld().getEntities();
+
+            for (Entity ent : entities) {
+                if(id == ent.getEntityId()) {
+                    return ent;
+                }
+            }
+            return null;
+        });
+
+        Bukkit.getScheduler().runTask(Atlas.getInstance(), task);
+
+        if(task.isDone()) {
+            try {
+                entity = task.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
             }
         }
     }
