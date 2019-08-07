ae4e3905a4f3fc0520032899cf4f6ced34bed0ba
funkemunky <funkemunkybiz@gmail.com>
2019-08-07 16:56:08 -0400

    Fixed bug with 1.7.10 packets

--- AtlasParent/Atlas/src/main/java/cc/funkemunky/api/tinyprotocol/api/packets/channelhandler/ChannelHandler1_7.java
+++ AtlasParent/Atlas/src/main/java/cc/funkemunky/api/tinyprotocol/api/packets/channelhandler/ChannelHandler1_7.java
@@ -54,14 +54,14 @@ public class ChannelHandler1_7 extends ChannelHandlerAbstract {
         }
 
         @Override public void write(net.minecraft.util.io.netty.channel.ChannelHandlerContext ctx, Object msg, net.minecraft.util.io.netty.channel.ChannelPromise promise) throws Exception {
-            Object packet = Atlas.getInstance().getTinyProtocolHandler().onPacketInAsync(player, msg);
+            Object packet = Atlas.getInstance().getTinyProtocolHandler().onPacketOutAsync(player, msg);
             if (packet != null) {
                 super.write(ctx, packet, promise);
             }
         }
 
         @Override public void channelRead(net.minecraft.util.io.netty.channel.ChannelHandlerContext ctx, Object msg) throws Exception {
-            Object packet = Atlas.getInstance().getTinyProtocolHandler().onPacketOutAsync(player, msg);
+            Object packet = Atlas.getInstance().getTinyProtocolHandler().onPacketInAsync(player, msg);
             if (packet != null) {
                 super.channelRead(ctx, packet);
             }
