b16a29fba68b7b93a3c675f0179f4d4e972d5cd7
Dawson Hessler <funkemunkybiz@gmail.com>
2019-10-29 07:26:35 -0400

    fixed update checker

--- AtlasParent/Atlas/src/main/java/cc/funkemunky/api/updater/Updater.java
+++ AtlasParent/Atlas/src/main/java/cc/funkemunky/api/updater/Updater.java
@@ -13,7 +13,7 @@ import java.nio.file.StandardCopyOption;
 
 @Getter
 public class Updater {
-    private int update = -1, currentUpdate = 48;
+    private int update = -1, currentUpdate = 50;
     private String version, downloadLink;
     private File pluginLocation;
     private boolean importantUpdate = true;
