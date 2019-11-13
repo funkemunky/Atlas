cd89fb3347636b104303157b8d916f1bc78549d2
funkemunky <funkemunkybiz@gmail.com>
2019-11-13 11:08:01 -0500

    Added user friendly error messages to MessageConfig

--- AtlasParent/Atlas/src/main/java/cc/funkemunky/api/config/MessageConfig.java
+++ AtlasParent/Atlas/src/main/java/cc/funkemunky/api/config/MessageConfig.java
@@ -2,10 +2,12 @@ package cc.funkemunky.api.config;
 
 import cc.funkemunky.api.utils.Color;
 import cc.funkemunky.api.utils.FunkeFile;
+import cc.funkemunky.api.utils.MiscUtils;
 import cc.funkemunky.carbon.utils.Pair;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class MessageConfig {
 
@@ -32,8 +34,19 @@ public class MessageConfig {
 
     private void load() {
         messages.clear();
+
+        for (int i = 0; i < file.getLines().size(); i++) {
+            String line = file.getLines().get(i);
+
+            int lineNumber = i; //For the sole purpose of using in the consumer below since it needs the int as final.
+            if(runCheck(line, reason -> MiscUtils.printToConsole("&cError while parsing message "
+                        + "&8(&f" + language + "&8) &con line " + lineNumber))) {
+                return;
+            }
+        }
+
         file.getLines().stream().map(line -> {
-            String[] split = line.split(": ", 2);
+            String[] split = line.replace("\"", "").split(": ", 2);
 
             return new Pair<>(split[0], split[1]);
         }).forEach(pair -> messages.put(pair.key, pair.value));
@@ -45,5 +58,20 @@ public class MessageConfig {
             save();
             return def;
         }));
+    }
+
+    //Returns true if the check failed.
+    private boolean runCheck(String line, Consumer<String> reason) {
+        if(line.length() == 0) {
+            reason.accept("Line is blank. Please remove this line.");
+            return true;
+        } else if(!line.contains(":")) {
+            reason.accept("No ':' was used.");
+            return true;
+        } else if(String.valueOf(line.charAt(0)).equals(":")) {
+            reason.accept("There is no key value (name) before ':' character.");
+            return true;
+        }
+        return false;
     }
 }
