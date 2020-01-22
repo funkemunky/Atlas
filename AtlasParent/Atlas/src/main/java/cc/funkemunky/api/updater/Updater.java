cc01d4c7624969f64c1583959bf1931c79d5b60d
Dawson Hessler <funkemunkybiz@gmail.com>
2020-01-22 17:02:14 -0500

    updated updater

--- AtlasParent/Atlas/src/main/java/cc/funkemunky/api/updater/Updater.java
+++ AtlasParent/Atlas/src/main/java/cc/funkemunky/api/updater/Updater.java
@@ -13,7 +13,7 @@ import java.nio.file.StandardCopyOption;
 
 @Getter
 public class Updater {
-    private int update = -1, currentUpdate = 57;
+    private int update = -1, currentUpdate = 58;
     private String version, downloadLink;
     private File pluginLocation;
     private boolean importantUpdate = true;
