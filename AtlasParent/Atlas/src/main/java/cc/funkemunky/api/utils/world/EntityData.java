05abd8b244d35cede58a2694126d4da81225ad11
Dawson Hessler <funkemunkybiz@gmail.com>
2020-07-09 12:56:22 -0400

    Updated box sizes as floats since this is more accurate to what it is

--- AtlasParent/Atlas/src/main/java/cc/funkemunky/api/utils/world/EntityData.java
+++ AtlasParent/Atlas/src/main/java/cc/funkemunky/api/utils/world/EntityData.java
@@ -55,29 +55,29 @@ public class EntityData {
     }
 
     static {
-        entityBounds.put(EntityType.WOLF, new SimpleCollisionBox(new Vector(), 0.62, 8));
-        entityBounds.put(EntityType.SHEEP, new SimpleCollisionBox(new Vector(), 0.9, 1.3));
-        entityBounds.put(EntityType.COW, new SimpleCollisionBox(new Vector(), 0.9, 1.3));
-        entityBounds.put(EntityType.PIG, new SimpleCollisionBox(new Vector(), 0.9, 0.9));
-        entityBounds.put(EntityType.MUSHROOM_COW, new SimpleCollisionBox(new Vector(), 0.9, 1.3));
-        entityBounds.put(EntityType.WITCH, new SimpleCollisionBox(new Vector(), 0.62, 1.95));
-        entityBounds.put(EntityType.BLAZE, new SimpleCollisionBox(new Vector(), 0.62, 1.8));
-        entityBounds.put(EntityType.PLAYER, new SimpleCollisionBox(new Vector(), 0.6, 1.8));
-        entityBounds.put(EntityType.VILLAGER, new SimpleCollisionBox(new Vector(), 0.62, 1.8));
-        entityBounds.put(EntityType.CREEPER, new SimpleCollisionBox(new Vector(), 0.62, 1.8));
-        entityBounds.put(EntityType.GIANT, new SimpleCollisionBox(new Vector(), 3.6, 10.8));
-        entityBounds.put(EntityType.SKELETON, new SimpleCollisionBox(new Vector(), 0.62, 1.8));
-        entityBounds.put(EntityType.ZOMBIE, new SimpleCollisionBox(new Vector(), 0.62, 1.8));
-        entityBounds.put(EntityType.SNOWMAN, new SimpleCollisionBox(new Vector(), 0.7, 1.9));
-        entityBounds.put(EntityType.HORSE, new SimpleCollisionBox(new Vector(), 1.4, 1.6));
-        entityBounds.put(EntityType.ENDER_DRAGON, new SimpleCollisionBox(new Vector(), 3, 1.5));
-        entityBounds.put(EntityType.ENDERMAN, new SimpleCollisionBox(new Vector(), 0.62, 2.9));
-        entityBounds.put(EntityType.CHICKEN, new SimpleCollisionBox(new Vector(), 0.4, 0.7));
-        entityBounds.put(EntityType.OCELOT, new SimpleCollisionBox(new Vector(), 0.62, 0.7));
-        entityBounds.put(EntityType.SPIDER, new SimpleCollisionBox(new Vector(), 1.4, 0.9));
-        entityBounds.put(EntityType.WITHER, new SimpleCollisionBox(new Vector(), 0.9, 3.5));
-        entityBounds.put(EntityType.IRON_GOLEM, new SimpleCollisionBox(new Vector(), 1.4, 2.9));
-        entityBounds.put(EntityType.GHAST, new SimpleCollisionBox(new Vector(), 4, 4));
+        entityBounds.put(EntityType.WOLF, new SimpleCollisionBox(new Vector(), 0.62f, .8f));
+        entityBounds.put(EntityType.SHEEP, new SimpleCollisionBox(new Vector(), 0.9f, 1.3f));
+        entityBounds.put(EntityType.COW, new SimpleCollisionBox(new Vector(), 0.9f, 1.3f));
+        entityBounds.put(EntityType.PIG, new SimpleCollisionBox(new Vector(), 0.9f, 0.9f));
+        entityBounds.put(EntityType.MUSHROOM_COW, new SimpleCollisionBox(new Vector(), 0.9f, 1.3f));
+        entityBounds.put(EntityType.WITCH, new SimpleCollisionBox(new Vector(), 0.62f, 1.95f));
+        entityBounds.put(EntityType.BLAZE, new SimpleCollisionBox(new Vector(), 0.62f, 1.8f));
+        entityBounds.put(EntityType.PLAYER, new SimpleCollisionBox(new Vector(), 0.6f, 1.8f));
+        entityBounds.put(EntityType.VILLAGER, new SimpleCollisionBox(new Vector(), 0.62f, 1.8f));
+        entityBounds.put(EntityType.CREEPER, new SimpleCollisionBox(new Vector(), 0.62f, 1.8f));
+        entityBounds.put(EntityType.GIANT, new SimpleCollisionBox(new Vector(), 3.6f, 10.8f));
+        entityBounds.put(EntityType.SKELETON, new SimpleCollisionBox(new Vector(), 0.62f, 1.8f));
+        entityBounds.put(EntityType.ZOMBIE, new SimpleCollisionBox(new Vector(), 0.62f, 1.8f));
+        entityBounds.put(EntityType.SNOWMAN, new SimpleCollisionBox(new Vector(), 0.7f, 1.9f));
+        entityBounds.put(EntityType.HORSE, new SimpleCollisionBox(new Vector(), 1.4f, 1.6f));
+        entityBounds.put(EntityType.ENDER_DRAGON, new SimpleCollisionBox(new Vector(), 3f, 1.5f));
+        entityBounds.put(EntityType.ENDERMAN, new SimpleCollisionBox(new Vector(), 0.62f, 2.9f));
+        entityBounds.put(EntityType.CHICKEN, new SimpleCollisionBox(new Vector(), 0.4f, 0.7f));
+        entityBounds.put(EntityType.OCELOT, new SimpleCollisionBox(new Vector(), 0.62f, 0.7f));
+        entityBounds.put(EntityType.SPIDER, new SimpleCollisionBox(new Vector(), 1.4f, 0.9f));
+        entityBounds.put(EntityType.WITHER, new SimpleCollisionBox(new Vector(), 0.9f, 3.5f));
+        entityBounds.put(EntityType.IRON_GOLEM, new SimpleCollisionBox(new Vector(), 1.4f, 2.9f));
+        entityBounds.put(EntityType.GHAST, new SimpleCollisionBox(new Vector(), 4f, 4f));
 
         if(ProtocolVersion.getGameVersion().isBelow(ProtocolVersion.V1_14)) {
             fieldWidth = entity.getFieldByName("width");
