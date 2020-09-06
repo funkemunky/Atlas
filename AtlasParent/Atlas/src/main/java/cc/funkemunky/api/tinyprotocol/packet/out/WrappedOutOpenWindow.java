d8fecb9fe6daa457905b7382e61505fdec8a3bcc
funkemunky <funkemunkybiz@gmail.com>
2020-09-06 18:22:02 -0400

    Fixed WrappedOutOpenWindow error in 1.14+

--- AtlasParent/Atlas/src/main/java/cc/funkemunky/api/tinyprotocol/packet/out/WrappedOutOpenWindow.java
+++ AtlasParent/Atlas/src/main/java/cc/funkemunky/api/tinyprotocol/packet/out/WrappedOutOpenWindow.java
@@ -23,20 +23,23 @@ public class WrappedOutOpenWindow extends NMSObject {
     }
 
     private static FieldAccessor<Integer> idField = fetchField(packet, int.class, 0);
-    private static FieldAccessor<String> nameField = fetchField(packet, String.class, 0);
+    private static FieldAccessor<String> nameField;
     private static FieldAccessor<Object> chatCompField;
     private static FieldAccessor<Integer> inventorySize = fetchField(packet, int.class, 1);
 
     private int id;
-    private String name;
+    private String name; //Not a thing in 1.14 and above.
     private WrappedChatComponent chatComponent;
     private int size;
 
     @Override
     public void process(Player player, ProtocolVersion version) {
         id = fetch(idField);
-        name = fetch(nameField);
         size = fetch(inventorySize);
+
+        if(ProtocolVersion.getGameVersion().isOrBelow(ProtocolVersion.V1_13_2)) {
+            name = fetch(nameField);
+        }
     }
 
     @Override
@@ -48,6 +51,9 @@ public class WrappedOutOpenWindow extends NMSObject {
         if(ProtocolVersion.getGameVersion().isOrAbove(ProtocolVersion.V1_8)) {
             chatCompField = fetchField(packet,
                     MinecraftReflection.iChatBaseComponent.getParent(), 0);
+        }
+        if(ProtocolVersion.getGameVersion().isOrBelow(ProtocolVersion.V1_13_2)) {
+            nameField = fetchField(packet, String.class, 0);
         }
     }
 }
