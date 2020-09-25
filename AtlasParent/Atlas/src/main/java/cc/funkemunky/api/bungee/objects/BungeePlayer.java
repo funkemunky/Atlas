3af69ac97544955907957f2e77a82ade15ddfc3f
Dawson Hessler <funkemunkybiz@gmail.com>
2020-09-25 09:55:40 -0400

    Fixing issues with 1.12 and bungee

--- AtlasParent/Atlas/src/main/java/cc/funkemunky/api/bungee/objects/BungeePlayer.java
+++ AtlasParent/Atlas/src/main/java/cc/funkemunky/api/bungee/objects/BungeePlayer.java
@@ -12,32 +12,10 @@ import java.util.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BungeePlayer {
-    public String name, server;
+    public String name, server, brand;
     public UUID uuid;
     public List<String> permissions;
     public Map<String, Object> values = new HashMap<>();
     public Map<String, String> forgeMods = new HashMap<>();
-    public boolean usingForge;
-
-    public static BungeePlayer fromJson(String json) {
-        try {
-            JSONObject object = new JSONObject(json);
-
-            BungeePlayer player = new BungeePlayer();
-
-            Iterator iterator = object.keys();
-
-            while(iterator.hasNext()) {
-                String key = (String) iterator.next();
-
-                Reflection.getField(BungeePlayer.class, key, 0).set(player, object.get(key));
-            }
-
-            player.uuid = Bukkit.getPlayer(player.name).getUniqueId();
-            return player;
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+    public boolean usingForge, legacy;
 }
