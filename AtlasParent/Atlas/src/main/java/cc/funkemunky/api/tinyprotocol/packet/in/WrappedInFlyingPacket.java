70d6c2ad063b5cf32c50a3eb0e0a1785fdfda112
Dawson Hessler <funkemunkybiz@gmail.com>
2021-07-06 10:47:31 -0400

    Adding back the ability to modify the PacketPlayInFlying

--- AtlasParent/Atlas/src/main/java/cc/funkemunky/api/tinyprotocol/packet/in/WrappedInFlyingPacket.java
+++ AtlasParent/Atlas/src/main/java/cc/funkemunky/api/tinyprotocol/packet/in/WrappedInFlyingPacket.java
@@ -6,6 +6,7 @@ package cc.funkemunky.api.tinyprotocol.packet.in;
 
 import cc.funkemunky.api.reflections.Reflections;
 import cc.funkemunky.api.reflections.types.WrappedClass;
+import cc.funkemunky.api.reflections.types.WrappedField;
 import cc.funkemunky.api.tinyprotocol.api.NMSObject;
 import cc.funkemunky.api.tinyprotocol.api.ProtocolVersion;
 import cc.funkemunky.api.tinyprotocol.packet.optimized.flying.AtlasPacketPlayInFlying;
@@ -15,6 +16,16 @@ import org.bukkit.entity.Player;
 @Getter
 public class WrappedInFlyingPacket extends NMSObject {
     private static final WrappedClass packet = Reflections.getNMSClass(Client.FLYING);
+
+    // Fields
+    private static final WrappedField fieldX = fetchField(packet, double.class, 0),
+            fieldY = fetchField(packet, double.class, 1),
+            fieldZ = fetchField(packet, double.class, 2),
+            fieldYaw = fetchField(packet, float.class, 0),
+            fieldPitch = fetchField(packet, float.class, 1),
+            fieldGround = fetchField(packet, boolean.class, 0),
+            hasPos = fetchField(packet, boolean.class, 1),
+            hasLook = fetchField(packet, boolean.class, 2);
 
     // Decoded data
     private double x, y, z;
@@ -40,6 +51,13 @@ public class WrappedInFlyingPacket extends NMSObject {
 
     @Override
     public void updateObject() {
-
+        set(fieldX, x);
+        set(fieldY, y);
+        set(fieldZ, z);
+        set(fieldYaw, yaw);
+        set(fieldPitch, pitch);
+        set(fieldGround, ground);
+        set(hasPos, pos);
+        set(hasLook, look);
     }
 }
