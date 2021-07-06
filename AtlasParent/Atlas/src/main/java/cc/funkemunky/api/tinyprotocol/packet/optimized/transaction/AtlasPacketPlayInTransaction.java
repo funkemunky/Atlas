072745e912e431ac8dfecb1167ece762ae09d7ed
Dawson Hessler <funkemunkybiz@gmail.com>
2021-07-06 10:55:43 -0400

    Implementing the start of transaction stuffs

--- AtlasParent/Atlas/src/main/java/cc/funkemunky/api/tinyprotocol/packet/optimized/transaction/AtlasPacketPlayInTransaction.java
+++ AtlasParent/Atlas/src/main/java/cc/funkemunky/api/tinyprotocol/packet/optimized/transaction/AtlasPacketPlayInTransaction.java
@@ -1,4 +1,17 @@
 package cc.funkemunky.api.tinyprotocol.packet.optimized.transaction;
 
-public class AtlasPacketPlayInTransaction {
+public abstract class AtlasPacketPlayInTransaction {
+
+    protected final Object packet;
+
+    public AtlasPacketPlayInTransaction(Object packet) {
+        this.packet = packet;
+    }
+
+    public abstract int getId();
+
+    public abstract short getAction();
+
+    public abstract boolean isAccepted();
+
 }
