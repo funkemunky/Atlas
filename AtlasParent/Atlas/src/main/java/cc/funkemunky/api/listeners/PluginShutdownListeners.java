311e44f6636dad9f2bbfe376dace12f93ad284ad
Dawson <30784509+funkemunky@users.noreply.github.com>
2022-05-10 09:26:19 -0400

    Fixing unregister for legacy commands system

--- AtlasParent/Atlas/src/main/java/cc/funkemunky/api/listeners/PluginShutdownListeners.java
+++ AtlasParent/Atlas/src/main/java/cc/funkemunky/api/listeners/PluginShutdownListeners.java
@@ -16,6 +16,7 @@ public class PluginShutdownListeners implements Listener {
             Atlas.getInstance().alog("&c" + event.getPlugin().getName() + "" +
                     " &7has been shutdown. Removing its hooks and listeners...");
             Atlas.getInstance().getFunkeCommandManager().removeAll(event.getPlugin());
+            Atlas.getInstance().getCommandManager(event.getPlugin()).unregisterCommands();
             Atlas.getInstance().getPacketProcessor().removeListeners(event.getPlugin());
             Atlas.getInstance().getBukkitCommandManager(event.getPlugin()).unregisterCommands();
             Atlas.getInstance().removePluginInstances(event.getPlugin());
