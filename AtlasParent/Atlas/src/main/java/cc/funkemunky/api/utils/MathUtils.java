12091cfe24dcd2749da88bc22b4f899f6a80c6b8
Dawson <30784509+funkemunky@users.noreply.github.com>
2022-05-24 09:42:41 -0400

    Adding a bunch of utils from Kauri

--- AtlasParent/Atlas/src/main/java/cc/funkemunky/api/utils/MathUtils.java
+++ AtlasParent/Atlas/src/main/java/cc/funkemunky/api/utils/MathUtils.java
@@ -1,5 +1,7 @@
 package cc.funkemunky.api.utils;
 
+import cc.funkemunky.api.tinyprotocol.packet.types.MathHelper;
+import lombok.val;
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -9,6 +11,8 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class MathUtils {
 
@@ -21,6 +25,375 @@ public class MathUtils {
 
     public static boolean playerMoved(Location from, Location to) {
         return playerMoved(from.toVector(), to.toVector());
+    }
+
+    public static double getDistanceWithoutRoot(KLocation one, KLocation two) {
+        double deltaX = one.x - two.x, deltaY = one.y - two.y, deltaZ = one.z - two.z;
+
+        return (deltaX * deltaX) + (deltaY * deltaY) + (deltaZ * deltaZ);
+    }
+
+    public static boolean isSameLocation(KLocation one, KLocation two) {
+        return one.x == two.x && one.y == two.y && one.z == two.z;
+    }
+    
+
+    public static double max(double... values) {
+        return Arrays.stream(values).max().orElse(Double.MAX_VALUE);
+    }
+
+    public static boolean isInteger(String string) {
+        try {
+            Integer.parseInt(string);
+            return true;
+        } catch(NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static int length(double value) {
+        return String.valueOf(value).length();
+    }
+
+    public static double getGrid(final Collection<Float> entry) {
+        double average = 0.0;
+        double min = 0.0, max = 0.0;
+
+        for (final double number : entry) {
+            if (number < min) min = number;
+            if (number > max) max = number;
+            average += number;
+        }
+
+        average /= entry.size();
+
+        return (max - average) - min;
+    }
+
+    //Skidded from Luke.
+    public static double getAngle(Location loc1, Location loc2) {
+        if (loc1 == null || loc2 == null) return -1;
+        Vector playerRotation = new Vector(loc1.getYaw(), loc1.getPitch(), 0.0f);
+        loc1.setY(0);
+        loc2.setY(0);
+        val rot = MathUtils.getRotations(loc1, loc2);
+        Vector expectedRotation = new Vector(rot[0], rot[1], 0);
+        return MathUtils.yawTo180D(playerRotation.getX() - expectedRotation.getX());
+    }
+
+    public static double getAngle(KLocation loc1, KLocation loc2) {
+        return getAngle(loc1.toLocation(null), loc2.toLocation(null));
+    }
+
+    public static float distanceBetweenAngles(float a, float b) {
+        final float first = a % 360;
+        final float second = b % 360;
+
+        final float delta = Math.abs(first - second);
+
+        return (float) Math.abs(Math.min(360.0 - delta, delta));
+    }
+
+    public static float getDistanceBetweenAngles(final float angle1, final float angle2) {
+        float distance = Math.abs(angle1 - angle2) % 360.0f;
+        if (distance > 180.0f) {
+            distance = 360.0f - distance;
+        }
+        return distance;
+    }
+
+    //Args: Tuple (a) is low outliers, Tupe (B) is high outliers
+    public static Tuple<List<Double>, List<Double>> getOutliers(List<Double> values) {
+
+        if(values.size() < 4) return new Tuple<>(new ArrayList<>(), new ArrayList<>());
+
+        double q1 = getMedian(values.subList(0, values.size() / 2)),
+                q3 = getMedian(values.subList(values.size() / 2, values.size()));
+        double iqr = Math.abs(q1 - q3);
+
+        double lowThreshold = q1 - 1.5 * iqr, highThreshold = q3 + 1.5 * iqr;
+
+        val tuple = new Tuple<List<Double>, List<Double>>(new ArrayList<>(), new ArrayList<>());
+
+        for (Double value : values) {
+            if(value < lowThreshold) tuple.one.add(value);
+            if(value < lowThreshold) tuple.one.add(value);
+            else if(value > highThreshold) tuple.two.add(value);
+        }
+
+        return tuple;
+    }
+
+    public static Tuple<List<Float>, List<Float>> getOutliersFloat(List<Float> values) {
+        if(values.size() < 4) return new Tuple<>(new ArrayList<>(), new ArrayList<>());
+
+        double q1 = getMedian(values.subList(0, values.size() / 2)),
+                q3 = getMedian(values.subList(values.size() / 2, values.size()));
+        double iqr = Math.abs(q1 - q3);
+
+        double lowThreshold = q1 - 1.5 * iqr, highThreshold = q3 + 1.5 * iqr;
+
+        val tuple = new Tuple<List<Float>, List<Float>>(new ArrayList<>(), new ArrayList<>());
+
+        for (Float value : values) {
+            if(value < lowThreshold) tuple.one.add(value);
+            else if(value > highThreshold) tuple.two.add(value);
+        }
+
+        return tuple;
+    }
+
+    public static Tuple<List<Long>, List<Long>> getOutliersLong(List<Long> collection) {
+        List<Long> values = new ArrayList<>(collection);
+
+        if(values.size() < 4) return new Tuple<>(new ArrayList<>(), new ArrayList<>());
+
+        double q1 = getMedian(values.subList(0, values.size() / 2)),
+                q3 = getMedian(values.subList(values.size() / 2, values.size()));
+        double iqr = Math.abs(q1 - q3);
+
+        double lowThreshold = q1 - 1.5 * iqr, highThreshold = q3 + 1.5 * iqr;
+
+        val tuple = new Tuple<List<Long>, List<Long>>(new ArrayList<>(), new ArrayList<>());
+
+        for (Long value : values) {
+            if(value < lowThreshold) tuple.one.add(value);
+            else if(value > highThreshold) tuple.two.add(value);
+        }
+
+        return tuple;
+    }
+
+    public static double getMedian(List<Double> data) {
+        if(data.size() > 1) {
+            if (data.size() % 2 == 0)
+                return (data.get(data.size() / 2) + data.get(data.size() / 2 - 1)) / 2;
+            else
+                return data.get(Math.round(data.size() / 2f));
+        }
+        return 0;
+    }
+    public static double getMedian(Iterable<? extends Number> iterable) {
+        List<Double> data = new ArrayList<>();
+
+        for (Number number : iterable) {
+            data.add(number.doubleValue());
+        }
+
+        return getMedian(data);
+    }
+
+    //Copied from apache math Kurtosis class.
+    public static double getKurtosisApache(Iterable<? extends Number> iterable) {
+        List<Double> values = new ArrayList<>();
+
+        double total = 0;
+        double kurt = Double.NaN;
+        for (Number number : iterable) {
+            double v = number.doubleValue();
+            total+= v;
+            values.add(v);
+        }
+
+        if(values.size() < 2) return kurt;
+
+        double mean = total / values.size();
+        double stdDev = MathUtils.stdev(values);
+        double accum3 = 0.0D;
+
+        for (Double value : values) {
+            accum3 += Math.pow(value - mean, 4.0D);
+        }
+
+        accum3 /= Math.pow(stdDev, 4.0D);
+        double n0 = values.size();
+        double coefficientOne = n0 * (n0 + 1.0D) / ((n0 - 1.0D) * (n0 - 2.0D) * (n0 - 3.0D));
+        double termTwo = 3.0D * Math.pow(n0 - 1.0D, 2.0D) / ((n0 - 2.0D) * (n0 - 3.0D));
+        kurt = coefficientOne * accum3 - termTwo;
+
+        return kurt;
+    }
+
+    public static double getKurtosis(final Iterable<? extends Number> iterable) {
+        double n = 0.0;
+        double n2 = 0.0;
+
+        for (Number number : iterable) {
+            n += number.doubleValue();
+            ++n2;
+        }
+
+        if (n2 < 3.0) {
+            return 0.0;
+        }
+        final double n3 = n2 * (n2 + 1.0) / ((n2 - 1.0) * (n2 - 2.0) * (n2 - 3.0));
+        final double n4 = 3.0 * Math.pow(n2 - 1.0, 2.0) / ((n2 - 2.0) * (n2 - 3.0));
+        final double n5 = n / n2;
+        double n6 = 0.0;
+        double n7 = 0.0;
+        for (final Number n8 : iterable) {
+            n6 += Math.pow(n5 - n8.doubleValue(), 2.0);
+            n7 += Math.pow(n5 - n8.doubleValue(), 4.0);
+        }
+        return n3 * (n7 / Math.pow(n6 / n2, 2.0)) - n4;
+    }
+
+    public static float pow(float number, int times) {
+        float answer = number;
+
+        if(times <= 0) return 0;
+
+        for(int i = 1 ; i < times ; i++) {
+            answer*= number;
+        }
+
+        return answer;
+    }
+
+    public static double varianceSquared(final Number n, final Iterable<? extends Number> iterable) {
+        double n2 = 0.0;
+        int n3 = 0;
+
+        for (Number number : iterable) {
+            n2 += Math.pow((number).doubleValue() - n.doubleValue(), 2.0);
+            ++n3;
+        }
+
+        return (n2 == 0.0) ? 0.0 : (n2 / (n3 - 1));
+    }
+
+    public static List<Double> getModes(final Iterable<? extends Number> iterable) {
+        List<Double> numbers = new ArrayList<>();
+
+        for (Number number : iterable) {
+            numbers.add(number.doubleValue());
+        }
+        final Map<Double, Long> countFrequencies = numbers.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        final double maxFrequency = countFrequencies.values().stream()
+                .mapToDouble(count -> count)
+                .max().orElse(-1);
+
+        return countFrequencies.entrySet().stream()
+                .filter(tuple -> tuple.getValue() == maxFrequency)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    //Copied from apache math Skewness class.
+    public static double getSkewnessApache(Iterable<? extends Number> iterable) {
+        List<Double> values = new ArrayList<>();
+
+        double total = 0;
+        double skew = Double.NaN;
+        for (Number number : iterable) {
+            double v = number.doubleValue();
+            total+= v;
+            values.add(v);
+        }
+
+        if(values.size() < 2) return skew;
+
+        double m = total / values.size();
+        double accum = 0.0D;
+        double accum2 = 0.0D;
+
+        for (Double value : values) {
+            double d = value - m;
+            accum += d * d;
+            accum2 += d;
+        }
+
+        double variance = (accum - accum2 * accum2 / values.size()) / (values.size() - 1);
+        double accum3 = 0.0D;
+
+        for (Double value : values) {
+            double d = value - m;
+            accum3 += d * d * d;
+        }
+
+        accum3 /= variance * Math.sqrt(variance);
+        double n0 = values.size();
+        skew = n0 / ((n0 - 1.0D) * (n0 - 2.0D)) * accum3;
+
+        return skew;
+    }
+
+    public static double getSkewness(final Iterable<? extends Number> iterable) {
+        double sum = 0;
+        int buffer = 0;
+
+        final List<Double> numberList = new ArrayList<>();
+
+        for (Number num : iterable) {
+            sum += num.doubleValue();
+            buffer++;
+
+            numberList.add(num.doubleValue());
+        }
+
+        Collections.sort(numberList);
+
+        final double mean =  sum / buffer;
+        final double median = (buffer % 2 != 0) ? numberList.get(buffer / 2) : (numberList.get((buffer - 1) / 2) + numberList.get(buffer / 2)) / 2;
+
+        return 3 * (mean - median) / deviationSquared(iterable);
+    }
+
+    public static double stdev(final Iterable<? extends Number> iterable) {
+        double sum = 0.0f;
+        double num = 0.0f;
+
+        final List<Double> list = new ArrayList<>();
+
+        for (Number number : iterable) {
+            list.add(number.doubleValue());
+        }
+
+        for (Double v : list) {
+            sum+= v;
+        }
+
+        double mean = sum / (float)list.size();
+
+        for (Double v : list) {
+            num+= Math.pow(v - mean, 2.0D);
+        }
+
+        return MathHelper.sqrt(num / (double)list.size());
+    }
+
+    public static double deviationSquared(final Iterable<? extends Number> iterable) {
+        double n = 0.0;
+        int n2 = 0;
+
+        for (Number anIterable : iterable) {
+            n += (anIterable).doubleValue();
+            ++n2;
+        }
+        final double n3 = n / n2;
+        double n4 = 0.0;
+
+        for (Number anIterable : iterable) {
+            n4 += Math.pow(anIterable.doubleValue() - n3, 2.0);
+        }
+
+        return (n4 == 0.0) ? 0.0 : (n4 / (n2 - 1));
+    }
+
+    public static int getDecimalCount(float number) {
+        return String.valueOf(number).split("\\.")[1].length();
+    }
+
+    public static int getDecimalCount(double number) {
+        return String.valueOf(number).split("\\.")[1].length();
+    }
+
+    public static float clampToVanilla(float s, float angle) {
+        float f = (s * 0.6f + .2f);
+        float f2 = f * f * f * 1.2f;
+        return angle - (angle % f2);
     }
 
     public static byte getByte(int num) {
