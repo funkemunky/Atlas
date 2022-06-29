538e8df29b85b3a715706d53635616d3e6713f6b
Dawson Hessler <funkemunkybiz@gmail.com>
2022-06-29 10:27:05 -0400

    Fixing package name

--- AtlasParent/Atlas/src/main/java/cc/funkemunky/api/utils/XMaterial.java
+++ AtlasParent/Atlas/src/main/java/cc/funkemunky/api/utils/XMaterial.java
@@ -20,7 +20,7 @@
  * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package com.cryptomorin.xseries;
+package cc.funkemunky.api.utils;
 
 import com.google.common.base.Enums;
 import com.google.common.cache.Cache;
